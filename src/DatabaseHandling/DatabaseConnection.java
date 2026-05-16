35d24f7bded28c39f7f6da87aae7425488d75dc5
Xaralampos Makridhs <makridhs.xaralampos@gmail.com>
2026-05-16 17:39:16 +0300

    Implementation of connection method to connect with database

---
+++ src/DatabaseHandling/DatabaseConnection.java
@@ -0,0 +1,26 @@
+package DatabaseHandling;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public final class DatabaseConnection {
+    private static final String DB_URL="jdbc:sqlite:PersonalFinanceManager/database/finance.db";
+
+    public static Connection getConnection(){
+        try{
+            Connection conn= DriverManager.getConnection(DB_URL);
+            enableForeignKeys(conn);
+            return conn;
+        }catch (SQLException e){
+            throw new RuntimeException("Cannot connect to database",e);
+        }
+    }
+
+    private static void enableForeignKeys(Connection conn) throws SQLException{
+        try(Statement statement=conn.createStatement()){
+            statement.execute("PRAGMA foreign_keys=ON");
+        }
+    }
+}
