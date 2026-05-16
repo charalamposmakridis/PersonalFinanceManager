38aeb70f7596c0227add5517221016368821dc54
Xaralampos Makridhs <makridhs.xaralampos@gmail.com>
2026-05-16 18:22:38 +0300

    debug .sql file

--- src/resources/schema.sql
+++ src/resources/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS users(
-    id INTEGER AUTOINCREMENT PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL UNIQUE,
     password_hash TEXT NOT NULL,
     full_name TEXT,
@@ -10,37 +10,37 @@ CREATE TABLE IF NOT EXISTS categories(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
     name TEXT NOT NULL,
-    type TEXT NOT NULL CHECK(type IN('INCOME','EXPENSE'))
-    created_at NOT NULL DEFAULT CURRENT_TIMESTAMP
+    type TEXT NOT NULL CHECK(type IN('INCOME','EXPENSE')),
+    created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
     UNIQUE(user_id,name,type)
-);
+    );
 
 CREATE TABLE IF NOT EXISTS transactions(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
     category_id INTEGER NOT NULL,
-    amount REAL NOT NULL CHECK(amount>0),
-    type TEXT NOT NULL CHECK(type IN ('INCOME','EXPENSE'))
+    amount REAL NOT NULL CHECK(amount > 0),
+    type TEXT NOT NULL CHECK(type IN ('INCOME','EXPENSE')),
     description TEXT,
-    transaction_date NOT NULL,
+    transaction_date TEXT NOT NULL,
     created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE RESTRICT
-);
+    FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY(category_id) REFERENCES categories(id) ON DELETE RESTRICT
+    );
 
 CREATE TABLE IF NOT EXISTS budgets(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
     category_id INTEGER NOT NULL,
     month INTEGER NOT NULL CHECK(month BETWEEN 1 AND 12),
-    year INTEGER NOT NULL CHECK(year>=2000),
-    limit_amount REAL NOT NULL CHECK(limit_amount>0),
+    year INTEGER NOT NULL CHECK(year >= 2000),
+    limit_amount REAL NOT NULL CHECK(limit_amount > 0),
 
     FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY(category_id) REFERNCES categories(id) ON DELETE CASCADE,
+    FOREIGN KEY(category_id) REFERENCES categories(id) ON DELETE CASCADE,
 
     UNIQUE(user_id,category_id,month,year)
-);
+    );
